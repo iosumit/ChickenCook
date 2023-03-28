f81554343f1eebc54f34c3852dd6ed5a4282e530
Sumit Kumar <sk4390789@gmail.com>
2023-03-28 17:54:54 +0530

    Cinemachine, Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
+    public bool IsWalking { get => isWalking; }
 
     void Update()
     {
@@ -28,8 +30,12 @@ public class Player : MonoBehaviour
         inputVector = inputVector.normalized;
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        isWalking = moveDir != Vector3.zero;
+
         transform.position += moveDir * Time.deltaTime * moveSpeed;
         float rotationSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
     }
+
 }
