6162d027e724068edcda4dc3f7992c7f66685ce7
Sumit Kumar <sk4390789@gmail.com>
2023-03-30 12:58:03 +0530

    scriptable objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
-    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
     public void Interact()
     {
-        Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
-        tomatoTransform.localPosition = Vector3.zero;
+        Transform kitchenObjTransorm = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjTransorm.localPosition = Vector3.zero;
     }
 }
