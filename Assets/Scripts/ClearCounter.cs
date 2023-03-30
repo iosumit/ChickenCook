661410e04a9c55e093ceb453fafb5bef8b15d940
Sumit Kumar <sk4390789@gmail.com>
2023-03-30 12:47:11 +0530

    spawn kitchen object on top of table

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private Transform counterTopPoint;
     public void Interact()
     {
-        Debug.Log(gameObject.name);
+        Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
+        tomatoTransform.localPosition = Vector3.zero;
     }
 }
